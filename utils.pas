f2559751e980ec6da6f1a829b5580b9a3f651d4b
DEntisT <zukanoviceltarik@gmail.com>
2022-02-02 10:18:45 +0100

    Update utils.pas

--- utils.pas
+++ utils.pas
@@ -1,8 +1,12 @@
+{program: start}
 program Hello;
+{header: vars}
 const variable = 1;
 var code: real  = 3.323 ;
-
+{code: start}
   begin
     writeln ('Hello.');
     writeln(code)
   end.
+{code: end}
+{program: end}
